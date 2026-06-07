6bda4bf3b7e4306e4f724dba986b1841bd9f5cef
koisoi <irisu.koi@gmail.com>
2026-06-07 17:06:03 +0300

    chaged address

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://koisoi.github.io',
-    base: '/old-ru-net'
+    base: '/oldrunet'
 });
--- src/const/consts.ts
+++ src/const/consts.ts
@@ -1,4 +1,4 @@
-export const base = '/old-ru-net';
+export const base = '/oldrunet';
 
 export const links = {
     chat: `${base}/chatroom`
