e9e5b481a8f9a441211cf6216554262ad46ce523
koisoi <irisu.koi@gmail.com>
2026-06-21 14:42:13 +0300

    main page v1 done

--- src/const/consts.ts
+++ src/const/consts.ts
@@ -1,5 +1,14 @@
 export const base = '/oldrunet';
 
 export const links = {
-    chat: `${base}/chatroom`
+    main: `${base}/`,
+    games: `${base}/games/`,
+    videos: `${base}/videos/`,
+    gallery: `${base}/gallery/`,
+    pages: `${base}/pages/`,
+    chat: `${base}/chatroom`,
+    forum: "https://oldrunet.bbtalk.me/"
 }
+
+// TODO: Add mail
+export const feedbackMail = 'example@mail.to'
